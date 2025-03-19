edd1452b37bba5fc55aa105fd3d2d65dac1d869d
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2025-03-19 21:47:08 +0700

    env, mvc, route, relationship

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,12 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('todo.index')" :active="request()->routeIs('todo.view')">
+                        {{ __('Todo') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('user.index')" :active="request()->routeIs('user.index')">
+                        {{ __('User') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -82,6 +88,12 @@
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
                     {{ __('Profile') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('todo.index')">
+                    {{ __('Todo') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('user.index')">
+                    {{ __('User') }}
                 </x-responsive-nav-link>
 
                 <!-- Authentication -->
